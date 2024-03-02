8245434570500f069f6e654cb4f4f31df02c6138
mikheyev <mikheyev9@gmail.com>
2024-03-02 16:47:31 +0300

    stage 1

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -29,6 +30,7 @@ class Post(models.Model):
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                 related_name='blog_posts')
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
